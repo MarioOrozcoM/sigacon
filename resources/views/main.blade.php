45e11ec4877d9e5d8d2d84d31c6446b47b04a5d6
Mario Daniel Orozco Muñoz <mariodanielorozco@hotmail.com>
2024-03-21 11:55:14 -0500

    uso de includes

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,36 +10,25 @@
 
 <!-- Inicio información dependiendo ROL -->
 
-<!-- superUsuario -->
+<!-- Inicio superUsuario -->
 @if(Auth::user()->rol === 'superUsuario')
         <!-- <p>¡Eres un superUsuario. Aquí está la información compartida.</p> -->
-    <header class="bg-black">
-    <div class="container mx-auto flex items-center justify-between px-4 py-2 text-white">
-        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="w-60">
-        
-        <div class="flex space-x-4 text-white text-lg">
-            <a href="{{ url('/main') }}" class="hover:text-gray-400">Inicio</a>
-            <a href="{{ url('/admin/users') }}" class="hover:text-gray-400">Usuarios</a>
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <button type="submit" class="hover:text-gray-400">Cerrar Sesión</button>
-            </form>
-        </div>
-    </div>
-    </header>
-    <!-- Inicio empresaRol -->
-    <div class="top-left-info ml-8 mt-4 text-lg text-semibold">
-        <p>{{ $user->rol }} - Empresa: - -</p>
-    </div>
-<!-- Cierre empresaRol -->
-<div class="text-center">
-    <h1 class="text-black text-2xl text-bold">Acciones</h1>
-</div>
-<!-- Inicio acciones disponibles -->
+        @include('superUsuario.mainSuper')  <!-- Agrego el include para no saturar el archivo actual -->
 
+    <!-- Inicio empresaRol -->
+        <div class="top-left-info ml-8 mt-4 text-lg text-semibold">
+            <p>{{ $user->rol }} - Empresa: - -</p>
+        </div>
+    <!-- Cierre empresaRol -->
+
+<!-- Inicio acciones disponibles -->
+    <div class="text-center">
+        <h1 class="text-black text-2xl text-bold">Acciones</h1>
+    </div>
 <!-- Cierre acciones disponibles -->
 
-<!-- superUsuario -->
+<!-- Cierre superUsuario -->
+
     @elseif(Auth::user()->rol === 'repreLegal' || Auth::user()->rol === 'juntaDirectiva' || Auth::user()->rol === 'revisorFiscal' || Auth::user()->rol === 'propietario' || Auth::user()->rol === 'proveedor' || Auth::user()->rol === 'cliente' || Auth::user()->rol === 'inmobiliaria')
         <p>¡Eres un repreLegal, juntaDirectiva, revisorFiscal, propietario, proveedor, cliente o inmobiliaria! Aquí está la información específica.</p>
     @elseif(Auth::user()->rol === 'normalUser')
---
+++ resources/views/superUsuario/mainSuper.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    
+<header class="bg-black">
+    <div class="container mx-auto flex items-center justify-between px-4 py-2 text-white">
+        <img src="{{ asset('images/logo.png') }}" alt="Logo" class="w-60">
+        
+        <div class="flex space-x-4 text-white text-lg">
+            <a href="{{ url('/main') }}" class="hover:text-gray-400">Inicio</a>
+            <a href="{{ url('/admin/users') }}" class="hover:text-gray-400">Usuarios</a>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="hover:text-gray-400">Cerrar Sesión</button>
+            </form>
+        </div>
+    </div>
+    </header>
+
+
+</body>
+</html>
